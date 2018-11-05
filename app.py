f3f51ae293881d7236be695624a12e8db162902a
RaymondDashWu <raymond31670@hotmail.com>
2018-11-04 20:45:14 -0800

    Heroku commit

--- app.py
+++ app.py
@@ -2,6 +2,14 @@ from flask import Flask
 # from histogram import *
 app = Flask(__name__)
 
+def gunicorn_app(environ, start_response):
+        data = b"Hello, World!\n"
+        start_response("200 OK", [
+            ("Content-Type", "text/plain"),
+            ("Content-Length", str(len(data)))
+        ])
+        return iter([data])
+
 @app.route('/')
 def hello_world():
     # print(frequency(unique_words("text-corpus.txt"),read_sterilize_source("text-corpus.txt")))
