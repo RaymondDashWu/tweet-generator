6a7ee0fe4b95470ab99ec2f4d85bc7967d8c029f
RaymondDashWu <raymond31670@hotmail.com>
2018-11-27 11:49:35 -0800

    Finished markov chain data structure

--- sample.py
+++ sample.py
@@ -1,5 +1,3 @@
-import dictogram
-
 def stochastic_sampling(words_dict):
     """Calculates total values of all words in dictionary, and then calculates 
     percentages to total 100%. 
@@ -23,52 +21,38 @@ def stochastic_sampling(words_dict):
     return min(words_dict, key=lambda y:abs(float(words_dict[y])-random_picker))
 
 def markov_chain(token_list):
-    # PSEUDO BRAINSTORM
-    # NOTE: DERP don't need to look for words in the back of a sentence. That's not how English works lol
-    # ['one', 'fish', 'two', 'fish', 'red', 'fish', 'blue', 'fish']
-    # iterate through each word while looking 4 ahead. lookahead arbitrary
-    # walk_steps = number of words to look forward 
-    # account for a start + end word aka START => TOKEN => WALK
-    # NOTE: NESTED dictionary that keeps track of each forward words
-    # nested dict format: {current word: {next_word = 1, next_next_word = 0,...}}
-    # Put each word + transitions in individual dictionary?
-    # weighted random selection
+    """Returns a data structure of word A followed by word B in a nested dictionary.
+    Ex: ['one', 'fish', 'two', 'fish', 'red', 'fish', 'blue', 'fish', 'two', 'fish']
+    {'one': {'fish': 1}, 'fish': {'two': 2, 'red': 1, 'blue': 1}, 'two': {'fish': 2}, 'red': {'fish': 1}, 'blue': {'fish': 1}}"""
     walk_steps = 1
     nest_dict = {}
     step_counter = 0
     total_steps = len(token_list)
 
     for i in range(total_steps - 1):
+        # Not necessary walk steps. Tried to account for longer more accurate lookaheads but don't know how to implement
         while step_counter < (walk_steps):
+            # returns a nested dictionary of word A which is followed by word B
+            # {'one': {'fish': 1}
             if token_list[i] not in nest_dict:
                 tmp_dict = {}
-                # token_list[i] = {tmp_dict[token_list[i]]: 1}
                 tmp_dict[token_list[i+1]] = 1
-                # TODO: Appending the word to dictionary of dictionary tmp_dict
-                # tmp_dict[index] = 
                 nest_dict[token_list[i]] = tmp_dict
             elif token_list[i] in nest_dict:
-                # Append the value in nested dictionary
-                # you might want another tmp_dict
-                # hold on to the value of that word
-                # figure out how to increment the value of the tmp_dict
-                tmp_dict = {}
-                print(nest_dict[token_list[i]])
-                tmp_dict = nest_dict[token_list[i]] += 1
-                # tmp_dict[token_list[nest_dict[i]]] += 1
+                # iterates through nested dictionary and adds to words in the nested dictionary
+                # ex: fish': {'two': 1} => fish': {'two': 2}
+                if token_list[i+1] in nest_dict[token_list[i]]:
+                    nest_dict[token_list[i]][token_list[i + 1]] += 1
+                else:
+                    nest_dict[token_list[i]][token_list[i + 1]] = 1
             step_counter += 1
         step_counter = 0
         print(nest_dict)
             
-                
-
-
-    # for word in token_list:
-    #     if index in range(len(token_list) - walk_steps):
-    #         print("hello")
+def markov_chain_walk()
 
 if __name__ == '__main__':
-  markov_chain(['one', 'fish', 'two', 'fish', 'red', 'fish', 'blue', 'fish'])             
+  markov_chain(['one', 'fish', 'two', 'fish', 'red', 'fish', 'blue', 'fish', 'two', 'fish'])             
 
 # TODO: Once Markov chain populated pick word after word
 
@@ -93,7 +77,3 @@ if __name__ == '__main__':
 
 #         # freq
 #                 return self.get(word, 0)
-
-
-# Don't know if this function is required yet
-# def markov_chain_walk()
