b183070c21068ffa2cde67fad5bfff7459406af1
RaymondDashWu <raymond31670@hotmail.com>
2018-12-14 04:03:09 -0800

    [HEROKU] Commit Attempt 9

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ from histogram import *
 app = Flask(__name__)
  @app.route('/')
 def hello_world():
-    print(frequency(unique_words("text-corpus.txt"),read_sterilize_source("text-corpus.txt")))
+    # print(frequency(unique_words("text-corpus.txt"),read_sterilize_source("text-corpus.txt")))
     # print(frequency(unique_words("text-corpus.txt"),read_sterilize_source("text-corpus.txt")))
     return "ello, gov'na!"
