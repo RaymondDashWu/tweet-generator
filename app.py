2a1a4a8759cf2a5dbb08cfd88e635a47d5dd0cf8
RaymondDashWu <raymond31670@hotmail.com>
2018-12-14 03:24:42 -0800

    [HEROKU] test commit 1

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import cleanup
 import tokenize
 import word_count
-import sample
+from sample import *
+# import sample
 import sentence
 
 from flask import Flask
@@ -10,15 +11,21 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    for _ in range(0,10):
-        return random.choice(unique_words(read_sterilize_source("text-corpus.txt")))
+    return sample
 
 if __name__ == "__main__":
-    with open("text-corpus.txt", "r") as file:
-        source = file.read()
-    # removes symbols, punctuation, etc. from source text and puts in array
-    sterilized_source = read_sterilize_source(source)
-    # retrieves uniques from above sterilized source
-    sterilized_source_uniques = unique_words(sterilized_source)
-    # counts the instances of uniques in sterilized source text
-    sterilized_source_frequency = frequency(sterilized_source_uniques, sterilized_source)
+    corpus = read_sterilize_source("sarcasm.txt")
+    chain = markov_chain_nth_order(corpus)
+    sentence_list = markov_chain_walk(chain)
+    print('sentence_list:', sentence_list)
+    
+    
+    
+    # with open("text-corpus.txt", "r") as file:
+    #     source = file.read()
+    # # removes symbols, punctuation, etc. from source text and puts in array
+    # sterilized_source = read_sterilize_source(source)
+    # # retrieves uniques from above sterilized source
+    # sterilized_source_uniques = unique_words(sterilized_source)
+    # # counts the instances of uniques in sterilized source text
+    # sterilized_source_frequency = frequency(sterilized_source_uniques, sterilized_source)
