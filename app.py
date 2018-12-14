d849be4d17919ffd5a73ac5ba732e81474c7ff02
RaymondDashWu <raymond31670@hotmail.com>
2018-12-14 04:02:05 -0800

    [HEROKU] Commit Attempt 8

--- app.py
+++ app.py
@@ -1,23 +1,9 @@
-import cleanup
-import tokenize
-import word_count
-import sample
-import sentence
- from flask import Flask
+from flask import Flask
 from histogram import *
+# from histogram import *
 app = Flask(__name__)
- 
  @app.route('/')
- def hello_world():
-    for _ in range(0,10):
-        return random.choice(unique_words(read_sterilize_source("text-corpus.txt")))
- print(hello_world()) 
-if __name__ == "__main__":
-    with open("text-corpus.txt", "r") as file:
-        source = file.read()
-    # removes symbols, punctuation, etc. from source text and puts in array
-    sterilized_source = read_sterilize_source(source)
-    # retrieves uniques from above sterilized source
-    sterilized_source_uniques = unique_words(sterilized_source)
-    # counts the instances of uniques in sterilized source text
-    sterilized_source_frequency = frequency(sterilized_source_uniques, sterilized_source)
+def hello_world():
+    print(frequency(unique_words("text-corpus.txt"),read_sterilize_source("text-corpus.txt")))
+    # print(frequency(unique_words("text-corpus.txt"),read_sterilize_source("text-corpus.txt")))
+    return "ello, gov'na!"
